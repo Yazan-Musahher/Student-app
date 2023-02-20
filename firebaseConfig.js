5cf8ef085184f7fed3cbac938d66ec5a356e249e
Mohamad Yazan Musahher <38183812+Yazan-Musahher@users.noreply.github.com>
2023-02-20 14:13:06 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import{addDoc, collection, getDoc, query, where, onSnapshot, getDocs} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAD1DfTbGFkOWbayOaWCL5VFNdZVbKuDE4",
-  authDomain: "student-app-a0860.firebaseapp.com",
-  projectId: "student-app-a0860",
-  storageBucket: "student-app-a0860.appspot.com",
-  messagingSenderId: "203960400565",
-  appId: "1:203960400565:web:4d86e15eba21a3bda31cfc"
+  apiKey: ""-----",",
+  authDomain: "-----",
+  projectId: "-----",
+  storageBucket: "-----",
+  messagingSenderId: "-----",
+  appId: "1:------"
 };
 
 // Initialize Firebase
